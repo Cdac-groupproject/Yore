b9a4be11f0b6b8f3f5a39dac1528bd33ae87b642
pranit08 <pranitkhandekar@gmail.com>
2025-08-06 14:18:22 +0530

    Auction Controller Bid Controller Modified

--- server/src/main/java/com/project/dto/AuctionRespDTO.java
+++ server/src/main/java/com/project/dto/AuctionRespDTO.java
@@ -3,10 +3,16 @@ package com.project.dto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuctionRespDTO {
 	private Long auctionId;
     private LocalDateTime startTime;
@@ -18,8 +24,8 @@ public class AuctionRespDTO {
     // Product Info
     private Long productId;
     private String productName;
-    private String productDescription;
-    private String productOriginCountry;
+    private String description;
+    private String countryOfOrigin;
 
     // Auctioneer Info
     private Long auctioneerId;
